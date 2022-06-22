17f935106311a293f0145a27d2510fde35b9ee20
Aron Adams <aronrapetto@hotmail.com>
2022-06-22 15:25:22 -0300

    props dos componentes button e input configuradas e validadas

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
+    const { buttonText, onClick } = this.props;
     return (
-      <button />
+      <button type="button" onClick={ onClick }>{ buttonText }</button>
     );
   }
 }
+
+Button.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Button;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Input extends React.Component {
   render() {
+    const { type, value, onChange, name, placeholder } = this.props;
     return (
-      <input />
+      <input
+        type={ type }
+        placeholder={ placeholder }
+        value={ value }
+        name={ name }
+        onChange={ onChange }
+      />
     );
   }
 }
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default Input;
