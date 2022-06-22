a02777c3486e9dfd1dd636728ca26658acdce8ce
Aron Adams <aronrapetto@hotmail.com>
2022-06-22 18:17:49 -0300

    corrigida estrutura de pastas, importações e testConfig

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import userLogin from '../store/actions';
+import userLogin from '../actions';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
