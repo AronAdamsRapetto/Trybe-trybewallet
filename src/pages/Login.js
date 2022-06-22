d93d0cee7abcba021ec3c84b15b502f34da57d4d
Aron Adams <aronrapetto@hotmail.com>
2022-06-22 15:30:52 -0300

    componentes adicionados a login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <form>
+          <Input
+            type="email"
+            placeholder="Digite seu e-mail"
+            name="email"
+            // value={ emailValue }
+            // onChange={ this.handleChange }
+          />
+          <Input
+            type="password"
+            placeholder="Digite sua senha"
+            name="password"
+            // value={ passwordValue }
+            // onChange={ this.handleChange }
+          />
+          <Button
+            buttonText="Entrar"
+            // onClick={ this.handleClick }
+          />
+        </form>
+      </main>
+    );
   }
 }
 
