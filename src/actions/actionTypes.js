13495e7d2567322af9fc7a5ed2d08be59dc8a5fe
Aron Adams <aronrapetto@hotmail.com>
2022-06-23 11:34:47 -0300

    actionTypes para add, remover e editar despesas criadas

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,6 @@
 export const LOGIN = 'LOGIN';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAIL = 'FETCH_FAIL';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
