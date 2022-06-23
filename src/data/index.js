d65364d051812fbcfbffa4e85bd4134c1a847da4
Aron Adams <aronrapetto@hotmail.com>
2022-06-23 14:41:55 -0300

    add lista com cabeçalho da tabela

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,14 @@
 export const paymentMethods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
 
 export const expenseTags = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+
+export const headerTable = [
+  'Descrição',
+  'Tag',
+  'Método de pagamento',
+  'Valor',
+  'Moeda',
+  'Câmbio convertido',
+  'Moeda de conversão',
+  'Editar/Excluir',
+];
